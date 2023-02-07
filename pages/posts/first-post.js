9a211dee0cb5a58f3bff698fe5d6aaf869f4e3b1
GrantLicata <grant.licata@gmail.com>
2023-02-06 20:52:21 -0800

    Layout component built and implemented

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,26 +1,19 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+import Layout from '../../components/layout';
+
 
 
 export default function FirstPost() {
     return (
-        <>
-            {/* Next.js includes a head component that allows you to specify the head of each page shown in the app. It is imported from the next library and provided within the page. */}
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                console.log(`script loaded correctly, window.FB has been populated`)
-                }
-            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
