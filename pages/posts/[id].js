b0360e92e21e2181882e87b4db72153fcb52bb91
GrantLicata <grant.licata@gmail.com>
2023-03-16 21:17:13 -0700

    Static path acquisition prepared

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,16 @@
 import Layout from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    // paths contains the array of known paths returned by getAllPostIds(), 
+    // which include the params defined by pages/posts/[id].js
+    return {
+        paths,
+        fallback: false,
+    };
+}
 
 export default function Post() {
-  return <Layout>...</Layout>;
+    return <Layout>...</Layout>;
 }
